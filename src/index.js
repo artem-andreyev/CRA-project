85eb357e3085f40944ad60d64caad4f6a82d4cd3
Artem <artem.andreyev212@gmail.com>
2024-06-19 21:36:33 +0300

    Added some scripts

--- src/index.js
+++ src/index.js
@@ -3,7 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const elem = <h2>Hello world!</h2>;
+const text = "Hello World!";
+
+const elem = (
+  <div>
+      <h2 className="text">Текст: {text}</h2>
+      <input type="text" />
+      <label htmlFor=""></label>
+      <button tabIndex="0">Click</button>
+  </div>
+);
 
 // const elem = React.createElement("h2", {className: "greetings"}, "Hello World!");
 
