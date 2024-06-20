5cda6ba4e612ba4b48783aaa57d587947713b23c
Artem <artem.andreyev212@gmail.com>
2024-06-20 21:43:43 +0300

    Added some scripts (classes)

--- src/index.js
+++ src/index.js
@@ -1,22 +1,11 @@
-import React from 'react';
+import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const text = "Hello World!";
-
-const elem = (
-  <div>
-      <h2 className="text">Текст: {text}</h2>
-      <input type="text" />
-      <label htmlFor=""></label>
-      <button tabIndex="0">Click</button>
-  </div>
-);
-
-// const elem = React.createElement("h2", {className: "greetings"}, "Hello World!");
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  elem
+  <StrictMode>
+      <App/>
+  </StrictMode>,
 );
